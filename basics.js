f5b3e5f7400e2ca30fe4e7b3ba04e545a9d3298c
LeaCrazyBambi <lheynitz@gmail.com>
2021-04-30 20:30:43 +0200

    :D

---
+++ basics.js
@@ -0,0 +1,35 @@
+const snackServings = 18;
+const guests = 5;
+
+console.log(snackServings + guests);
+console.log(snackServings - guests);
+console.log(guests - snackServings);
+console.log(snackServings * guests);
+console.log(snackServings / guests);
+
+const drinks = 10;
+
+const resultOne = snackServings * guests / drinks;
+console.log(resultOne);
+
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);
+
+const monkeyfights = 20;
+const resultTwo = (monkeys + bananas) * monkeyfights;
+console.log(resultTwo);
+
+monkeys++;
+console.log(monkeys);
+
+bananas--;
+console.log(bananas);
+
+const happiness = bananas - monkeys;
+console.log(happiness + monkeyfights);
+
+console.log(resultOne % resultTwo);
+
+
+
